9b24d76a60b878db24846b2d10d2cb2ab1a78758
gzc <2964776559@qq.com>
2024-07-03 00:06:23 +0800

    Description：add atten and feedforward part Author：gzc Date：2024/7/3

---
+++ Transformer_components/Feedforward.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_inner_hid=512, dropout=0.1):
+        super(FeedForward, self).__init__()
+        self.seq = nn.Sequential(
+            nn.LayerNorm(d_model, 1e-6),
+            nn.Linear(d_model, d_inner_hid),
+            nn.ReLU(inplace=True),
+            nn.Dropout(dropout),
+            nn.Linear(d_inner_hid, d_model),
+            nn.Dropout(dropout)
+            )
+    
+    def forward(self, inputs: Tensor):
+    return self.seq(inputs)
---
+++ Transformer_components/self_attention.py
@@ -0,0 +1,31 @@
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+Class Multi_head_attention(nn.Module):
+    def __init__(self, d_model, n_head, dropout=0.1):
+        super(Multi_head_attention, self).__init__()
+        self.d_model = d_model
+        self.n_head = n_head
+        self.d_k = d_model // n_head
+        self.linear =clones(nn.Linear(d_model, d_model) , 4)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, q, k, v, mask=None):
+        batch_size = q.size(0)
+        q,k,v =[ l(x).view(batch_size, -1, self.n_head, self.d_k).transpose(1, 2) for l, x in zip(self.linear, [q, k, v])]
+        scores = torch.matmul(q, k.transpose(-2, -1)) / torch.sqrt(self.d_k)
+        if mask is not None:
+            scores = scores.masked_fill(mask==0, -1e9)
+        p_attn = F.softmax(scores, dim=-1)
+        p_attn = self.dropout(p_attn)
+        x = torch.matmul(p_attn, v)
+        #x （batch_size, n_head, seq_len, d_k），先转成(batch_size, seq_len, n_head * d_k)，即(batch_size, seq_len, d_model)
+        #通过线性转化输出
+        x = x.transpose(1, 2).contiguous().view(batch_size, -1, self.n_head * self.d_k)
+        return  self.linear[-1](x)
